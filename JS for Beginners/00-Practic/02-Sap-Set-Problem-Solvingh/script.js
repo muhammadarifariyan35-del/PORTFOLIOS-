fc3aa52da510190304858e5a2cff09a55a9f1374
Arif Ariyan <muhammadarifariyan35@gmail.com>
2026-06-24 11:38:18 +0600

    Map Set provlem solving

---
+++ JS for Beginners/00-Practic/02-Sap-Set-Problem-Solvingh/script.js
@@ -0,0 +1,29 @@
+const dirtyRolls = [10, 12, 10, 15, 12, 20, 15, 30];
+const removeDuplicat = new Set(dirtyRolls);
+const array = [...removeDuplicat];
+console.log(array);
+
+const userDataBase = new Map();
+userDataBase.set(101, "Anik");
+userDataBase.set(102, "Sumi");
+userDataBase.set(103, "Tamim");
+console.log(userDataBase.has(102), userDataBase.get(101));
+userDataBase.delete(101);
+console.log(userDataBase);
+
+const asifHobbies = new Set(["Coding", "Gaming", "Reading", "Biking"]);
+const ronyHobbies = new Set(["Gaming", "Traveling", "Biking", "Cooking"]);
+const commonHobbis = asifHobbies.intersection(ronyHobbies);
+console.log(commonHobbis);
+
+const partyTracker = new Map([
+  ["Rahim", 3],
+  ["Karim", 2],
+  ["Sujon", 4],
+]);
+let totaldrinks = 0;
+for (let [name, drinks] of partyTracker) {
+  console.log(`${name} has consumed ${drinks} drinks.`);
+  totaldrinks += drinks;
+}
+console.log(`Total drinks consumed in the party: ${totaldrinks}`);
