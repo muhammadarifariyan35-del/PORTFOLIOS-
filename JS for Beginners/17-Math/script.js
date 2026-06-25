52ad947994d79e1410b5eb22f7ac048e6076ed9c
Arif Ariyan <muhammadarifariyan35@gmail.com>
2026-06-25 21:04:46 +0600

    Math practic

---
+++ JS for Beginners/17-Math/script.js
@@ -0,0 +1,75 @@
+//* Math Properties (Constants)
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.SQRT2);
+console.log(Math.SQRT1_2);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
+
+//* Math.round()
+console.log(Math.round(4.4));
+console.log(Math.round(4.8));
+
+//* Math.ceil()
+console.log(Math.ceil(44.0001));
+console.log(Math.ceil(-44.0001));
+
+//* Math.floor()
+console.log(Math.floor(44.99));
+console.log(Math.floor(-44.99));
+
+//* Math.trunc()
+console.log(Math.trunc(4.9));
+console.log(Math.trunc(4.2));
+console.log(Math.trunc(-4.2));
+
+//* Math.sign()
+console.log(Math.sign(-4));
+console.log(Math.sign(4));
+
+//* Math.pow()
+console.log(Math.pow(8, 2));
+console.log(Math.pow(20, 3));
+console.log(Math.pow(10, 5));
+
+//* Math.sqrt()
+console.log(Math.sqrt(64));
+console.log(Math.sqrt(100));
+
+//* Math.abs()
+console.log(Math.abs(-44));
+console.log(Math.abs(-9873));
+
+//* Math.sin()
+console.log(Number(Math.sin((90 * Math.PI) / 180).toFixed(2)));
+console.log(Number(Math.sin((60 * Math.PI) / 180).toFixed(2)));
+console.log(Number(Math.sin((180 * Math.PI) / 180).toFixed(2)));
+console.log(Number(Math.sin((360 * Math.PI) / 180).toFixed(2)));
+
+//* Math.cos()
+console.log(Number(Math.cos((90 * Math.PI) / 180).toFixed(2)));
+console.log(Number(Math.cos((60 * Math.PI) / 180).toFixed(2)));
+console.log(Number(Math.cos((180 * Math.PI) / 180).toFixed(2)));
+console.log(Number(Math.cos((360 * Math.PI) / 180).toFixed(2)));
+
+//* Math.min()
+console.log(Math.min(0, 150, 30, 20, -8, -200));
+
+//* Math.max()
+console.log(Math.max(0, 150, 30, 20, -8, -200));
+
+//* Math.random()
+console.log(Math.trunc(5000 + Math.random() * 5000));
+console.log(Math.trunc(1000 + Math.random() * 9999));
+
+//* Math.log()
+console.log(Math.log(1));
+console.log(Math.log(Math.E));
+
+//* Math.log2()
+console.log(Math.log2(8));
+
+//* Maht.log10()
+console.log(Math.log10(10000000000));
