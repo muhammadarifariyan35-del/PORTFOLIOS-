e16c69462019d4f20c28174b67dd01e108c558f9
Arif Ariyan <muhammadarifariyan35@gmail.com>
2026-07-22 16:27:06 +0600

    finishing Errors

--- JS for Beginners/20-Errors/script.js
+++ JS for Beginners/20-Errors/script.js
@@ -31,3 +31,36 @@ try {
 } catch (err) {
   console.log(err.name + " " + err.description);
 }
+
+//!===
+
+let result = "Not Active.";
+let isActive = false;
+if (isActive == true) {
+  result = "Active!";
+}
+console.log(result);
+
+//*---
+
+function myFunction() {
+  const message = document.getElementById("p01");
+  message.innerHTML = "";
+  let x = document.getElementById("demo").value;
+
+  try {
+    if (x.trim() == "") throw "empty";
+    if (isNaN(x)) throw "not a number";
+
+    x = Number(x);
+    if (x < 5) throw "too low";
+    if (x > 10) throw "too high";
+    else {
+      throw "Okay";
+    }
+  } catch (err) {
+    message.innerHTML = "Input is " + err;
+  }finally{
+    document.getElementById("demo").value = ""
+  }
+}
