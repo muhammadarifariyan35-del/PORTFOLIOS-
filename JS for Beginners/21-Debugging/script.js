30719ad4619c174e9970e122f6873cd71d4887fa
Arif Ariyan <muhammadarifariyan35@gmail.com>
2026-07-23 15:17:02 +0600

    dibugging

---
+++ JS for Beginners/21-Debugging/script.js
@@ -0,0 +1,9 @@
+let price = 50;
+let quantity = 3;
+let total = price * quantity;
+
+console.log("Total:", total);
+console.warn("এই ফিচারটি পরবর্তীতে তুলে নেওয়া হতে পারে!");
+console.error("ডাটাবেজ কানেকশন ব্যর্থ হয়েছে!");
+console.log("first")
+debugger;
