3e034b521301811a529fd4c5d476c9448f7926b8
Arif Ariyan <muhammadarifariyan35@gmail.com>
2026-08-24 18:53:14 +0600

    Document object mode

--- JS for Beginners/24-Dom/script.js
+++ JS for Beginners/24-Dom/script.js
@@ -2,7 +2,48 @@ document.getElementById("demo").innerHTML = "hello istar jahan jui";
 
 function newFunction() {
   document.querySelector("p").style.fontSize = "xx-large";
-  document.querySelector("p").style.color = "rgba(1, 163, 164,1.0)"
+  document.querySelector("p").style.color = "rgba(1, 163, 164,1.0)";
   document.querySelector("#demo").innerHTML =
     "hello <strong>Istar</strong> jahan";
 }
+
+let me = document.getElementById("me");
+console.log(me.parentElement);
+console.log(me.nextElementSibling);
+console.log(me.previousElementSibling);
+
+const family = document.getElementById("family");
+console.log(family.children);
+
+let newTag = document.createElement("li");
+newTag.innerText = "new Item";
+document.getElementById("myList").appendChild(newTag);
+// newTag.remove();
+
+let newParagraph = document.createElement("p");
+newParagraph.innerText = "Hello my name is arif ariyan";
+newParagraph.setAttribute("id", "myPara");
+document.querySelector("body").append(newParagraph);
+
+// ১. আগে এলিমেন্ট দুটোকে ধরে নিলাম
+const btn = document.getElementById("myBtn");
+const text = document.getElementById("text");
+
+// ২. Event Listener বসানো
+btn.addEventListener("keydown", function () {
+  text.innerText = "তুমি বাটনে ক্লিক করেছ! ";
+});
+
+const myinput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", function () {
+  if (myinput.value.trim() === "") {
+    return alert("দয়া করে কিছু লেখো!");
+  }
+
+  let li = document.createElement("li");
+  li.innerText = myinput.value;
+  li.setAttribute("class", "taskListLI");
+  document.getElementById("taskList").appendChild(li);
+});
