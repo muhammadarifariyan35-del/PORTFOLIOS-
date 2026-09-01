fd626e212db1b2436fd365d41d0076859321026b
Arif Ariyan <muhammadarifariyan35@gmail.com>
2026-09-01 12:32:04 +0600

    new

--- JS for Beginners/26-Events/script.js
+++ JS for Beginners/26-Events/script.js
@@ -5,3 +5,12 @@ btn.addEventListener("mousemove", function () {
   let date = new Date();
   document.getElementById("demo").innerText = date.toLocaleString();
 });
+
+//! --------------
+
+let cli = document.getElementById("clint");
+
+cli.addEventListener("mousemove", function (e) {
+  document.getElementById("clintPara").innerText =
+    `X axis: ${e.clientX}, Y axis: ${e.clientY}`;
+});
