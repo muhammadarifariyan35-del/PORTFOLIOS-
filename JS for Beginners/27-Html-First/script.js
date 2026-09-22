2f998ee16ec418f2aa2cf000ff8ab4b55c53a472
Arif Ariyan <muhammadarifariyan35@gmail.com>
2026-09-22 17:13:52 +0600

    new

---
+++ JS for Beginners/27-Html-First/script.js
@@ -0,0 +1,9 @@
+const activeItem = document.getElementById("description");
+
+console.log(activeItem.parentElement.innerHTML);
+
+console.log(activeItem.nextElementSibling.innerHTML);
+
+console.log(activeItem.previousElementSibling.innerHTML);
+
+console.log(activeItem.innerHTML);
