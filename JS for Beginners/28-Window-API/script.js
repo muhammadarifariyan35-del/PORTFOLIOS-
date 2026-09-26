3bec0919353061d68ef37d0856f0e1f9d3628b9e
Arif Ariyan <muhammadarifariyan35@gmail.com>
2026-09-26 15:52:12 +0600

    window API

---
+++ JS for Beginners/28-Window-API/script.js
@@ -0,0 +1,15 @@
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+console.log(`width ${width}px height ${height}px`);
+
+console.log(screen.width);
+console.log(screen.height);
+console.log(screen.availWidth);
+console.log(screen.availHeight);
+
+console.log(window.location)
+console.log(location.protocol)
+console.log(location.hostname)
+console.log(location.port)
+console.log(location.pathname)
